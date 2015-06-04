bc0be182f9b840622941538b3c9d631ad96b2b7d
A. Samy <f.fallen45@gmail.com>
2015-06-04 20:17:33 +0000

    Initial working testcase

--- test.c
+++ test.c
@@ -4,11 +4,20 @@
 
 extern void *htable_init(size_t);
 extern void htable_free(void *table);
-extern void htable_add(void *, const void *, size_t, size_t);
+extern void htable_add(void *, size_t, const void *);
 extern void *htable_get(void *, size_t);
 
 extern size_t htable_size(const void *);
 extern size_t htable_cap(const void *);
+
+static __attribute__((noinline)) size_t hash_u32(const void *p)
+{
+	uintptr_t tmp = (uintptr_t)p;
+	tmp = ((tmp >> 16) ^ tmp) * 0x45d9f3b;
+	tmp = ((tmp >> 16) ^ tmp) * 0x45d9f3b;
+	tmp = ((tmp >> 16) ^ tmp);
+	return tmp;
+}
 
 int main()
 {
@@ -23,12 +32,13 @@ int main()
 		ptr[i] = malloc(sizeof(uint32_t));
 		*ptr[i] = i;
 		printf("htable_add(%p, %d)\n", ptr[i], i);
-		htable_add(ht, ptr[i], i, 0xdeadbeef);
+		htable_add(ht, hash_u32(ptr[i]), ptr[i]);
 	}
 
 	for (i = 0; i < htable_size(ht); ++i) {
 		uint32_t *tmp = htable_get(ht, i);
 
+		printf("%p\n", tmp);
 		printf("htable_get(%d): %p => *%d(original: %p same: %s)\n",
 			i, tmp, ptr[i], *tmp, ptr[i] == tmp ? "yes" : "nop"
 		);
